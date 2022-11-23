8f8dc10adf3fc420359501eae5473406df7ac937
Jar <114672583+ddys001@users.noreply.github.com>
2022-11-23 16:20:45 +0800

    Add files via upload

--- js/cokemv.js
+++ js/cokemv.js
@@ -1,6 +1,5 @@
-var rule = {
+var rule = Object.assign(muban.mxpro,{
 title:'cokemv',
-模板:'mxpro',
 host:'https://cokemv.me',
 class_parse:'.navbar-items li:gt(1):lt(7);a&&Text;a&&href;/(\\d+).html',
-}
+});
